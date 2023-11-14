9dae70590730d26355569dd6f3a4f099706fb156
Vladyslav Mankivskyi <mankivskiy.vlsd@gmail.com>
2023-11-14 09:39:55 +0200

    add config validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { AtGuard } from './common/guards';
 
 @Module({
-  imports: [AuthModule, PrismaModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        DATABASE_URL: Joi.string(),
+        AT_SECRET: Joi.string(),
+        RT_SECRET: Joi.string(),
+      }),
+      validationOptions: {
+        allowUnknown: false,
+        abortEarly: true,
+      },
+    }),
+    AuthModule,
+    PrismaModule,
+  ],
   providers: [
     {
       provide: 'APP_GUARD',
